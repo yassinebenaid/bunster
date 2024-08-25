e005dcd4a51b50b79d1a722841973db0ec75bfff
yassinebenaid <yassinebenaide3@gmail.com>
2024-08-25 13:04:21 +0100

    parser: added simple command parser

--- parser/parser.go
+++ parser/parser.go
@@ -7,32 +7,31 @@ import (
 )
 
 func New(l lexer.Lexer) Parser {
-	var p = Parser{
-		l: l,
-	}
+	var p = Parser{l: l}
+
+	// So that both curr and next tokens get initialized.
+	p.proceed()
+	p.proceed()
+
 	return p
 }
 
 type Parser struct {
-	l            lexer.Lexer
-	currentToken token.Token
-	nextToken    token.Token
+	l    lexer.Lexer
+	curr token.Token
+	next token.Token
 }
 
 func (p *Parser) proceed() {
-	p.currentToken = p.nextToken
-	p.nextToken = p.l.NextToken()
+	p.curr = p.next
+	p.next = p.l.NextToken()
 }
 
 func (p *Parser) ParseScript() ast.Script {
 	var script ast.Script
 
-loop:
-	for {
-		p.proceed()
-		switch p.currentToken.Type {
-		case token.EOF:
-			break loop
+	for ; p.curr.Type != token.EOF; p.proceed() {
+		switch p.curr.Type {
 		default:
 			script.Statements = append(script.Statements, p.parseCommand())
 		}
@@ -44,18 +43,18 @@ loop:
 func (p *Parser) parseCommand() ast.Command {
 	var cc ast.Command
 
-	cc.Name = p.currentToken.Literal
+	cc.Name = p.curr.Literal
 
 	for {
-		if p.nextToken.Type == token.BLANK {
+		if p.next.Type == token.BLANK {
 			p.proceed()
 		}
-		if p.nextToken.Type != token.Word {
+		if p.next.Type != token.Word {
 			break
 		}
 
 		p.proceed()
-		cc.Args = append(cc.Args, p.currentToken.Literal)
+		cc.Args = append(cc.Args, p.curr.Literal)
 	}
 
 	return cc
