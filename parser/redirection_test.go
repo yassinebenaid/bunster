4df85685ebe2828971299a5d3c1ecb2572e0f138
yassinebenaid <yassinebenaide3@gmail.com>
2024-09-21 15:00:13 +0100

    parser: clean unecessary tests

--- parser/redirection_test.go
+++ parser/redirection_test.go
@@ -21,44 +21,6 @@ var redirectionTests = []testCase{
 			},
 		},
 	}},
-	{`cmd > /dev/null arg1 >foobar.txt arg2 >$var arg3 >'some string goes here' arg>/dev/null`, ast.Script{
-		Statements: []ast.Node{
-			ast.Command{
-				Name: ast.Word("cmd"),
-				Args: []ast.Node{
-					ast.Word("arg1"),
-					ast.Word("arg2"),
-					ast.Word("arg3"),
-					ast.Word("arg"),
-				},
-				Redirections: []ast.Redirection{
-					{Src: ast.FileDescriptor("1"), Method: ">", Dst: ast.Word("/dev/null")},
-					{Src: ast.FileDescriptor("1"), Method: ">", Dst: ast.Word("foobar.txt")},
-					{Src: ast.FileDescriptor("1"), Method: ">", Dst: ast.SimpleExpansion("var")},
-					{Src: ast.FileDescriptor("1"), Method: ">", Dst: ast.Word("some string goes here")},
-					{Src: ast.FileDescriptor("1"), Method: ">", Dst: ast.Word("/dev/null")},
-				},
-			},
-		},
-	}},
-	{`cmd>"foo $bar baz">'boo'`, ast.Script{
-		Statements: []ast.Node{
-			ast.Command{
-				Name: ast.Word("cmd"),
-				Args: nil,
-				Redirections: []ast.Redirection{
-					{Src: ast.FileDescriptor("1"), Method: ">", Dst: ast.Concatination{
-						Nodes: []ast.Node{
-							ast.Word("foo "),
-							ast.SimpleExpansion("bar"),
-							ast.Word(" baz"),
-						},
-					}},
-					{Src: ast.FileDescriptor("1"), Method: ">", Dst: ast.Word("boo")},
-				},
-			},
-		},
-	}},
 	// {`cmd >> /dev/null arg1 >>foobar.txt arg2 >>$var arg3 >>'some string goes here' arg4`, ast.Script{
 	// 	Statements: []ast.Node{
 	// 		ast.Command{
