cfd5a096a95cf6cd42d5ba02378449d5c5a3876e
yassinebenaid <yassinebenaide3@gmail.com>
2024-09-21 20:12:13 +0100

    parser: handle stdin redirection to fd

--- parser/redirection_test.go
+++ parser/redirection_test.go
@@ -83,7 +83,7 @@ var redirectionTests = []testCase{
 			},
 		},
 	}},
-	{`cmd<'file.ext' arg < file</foo/bar`, ast.Script{
+	{`cmd<'file.ext' arg < file</foo/bar  3<foo.bar`, ast.Script{
 		Statements: []ast.Node{
 			ast.Command{
 				Name: ast.Word("cmd"),
@@ -93,6 +93,7 @@ var redirectionTests = []testCase{
 					{Src: ast.FileDescriptor("0"), Method: "<", Dst: ast.Word("file.ext")},
 					{Src: ast.FileDescriptor("0"), Method: "<", Dst: ast.Word("file")},
 					{Src: ast.FileDescriptor("0"), Method: "<", Dst: ast.Word("/foo/bar")},
+					{Src: ast.FileDescriptor("3"), Method: "<", Dst: ast.Word("foo.bar")},
 				},
 			},
 		},
