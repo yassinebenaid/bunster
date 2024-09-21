e11d47c0cf3b990df0b2fd8d9f2947d6d6c23957
yassinebenaid <yassinebenaide3@gmail.com>
2024-09-21 20:37:49 +0100

    parser: handle more test cases

--- parser/redirection_test.go
+++ parser/redirection_test.go
@@ -102,7 +102,7 @@ var redirectionTests = []testCase{
 			},
 		},
 	}},
-	{`cmd<<<'foo bar' arg <<< foo<<<foo-bar arg2 <<<"$var" 3<<<foobar`, ast.Script{
+	{`cmd<<<'foo bar' arg <<< foo<<<foo-bar arg2 <<<"$var" 3<<<foobar <<<123`, ast.Script{
 		Statements: []ast.Node{
 			ast.Command{
 				Name: ast.Word("cmd"),
@@ -115,6 +115,7 @@ var redirectionTests = []testCase{
 					{Src: ast.FileDescriptor("0"), Method: "<<<", Dst: ast.Word("foo-bar")},
 					{Src: ast.FileDescriptor("0"), Method: "<<<", Dst: ast.SimpleExpansion("var")},
 					{Src: ast.FileDescriptor("3"), Method: "<<<", Dst: ast.Word("foobar")},
+					{Src: ast.FileDescriptor("0"), Method: "<<<", Dst: ast.Word("123")},
 				},
 			},
 		},
