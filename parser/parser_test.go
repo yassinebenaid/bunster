2d0ef5f55931b1a4ab9a4a504d8dcd428df7e3bf
yassinebenaid <yassinebenaide3@gmail.com>
2024-09-22 15:42:43 +0100

    parser: handle  missing filename after "fd>> fd>>file" redirection

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -260,8 +260,8 @@ var errorHandlingTestCases = []errorHandlingTestCase{
 
 	{`cmd 1>`, "syntax error: a file name was not provided after the `>`."},
 	{`cmd 1> 1>x`, "syntax error: a file name was not provided after the `>`."},
-	// // {`cmd >>`, "syntax error: a file name was not provided after the `>>`."},
-	// // {`cmd >&`, "syntax error: a file descriptor was not provided after the `>&`."},
+	{`cmd 1>> 1>>x`, "syntax error: a file name was not provided after the `>>`."},
+	// {`cmd 1>&`, "syntax error: a file descriptor was not provided after the `>&`."},
 	// // {`cmd >&foo`, "syntax error: a file descriptor was not provided after the `>&`."},
 	// // {`cmd >& foo`, "syntax error: a file descriptor was not provided after the `>&`."},
 }
