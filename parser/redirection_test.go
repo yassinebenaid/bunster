588d957b4ac58feb686ef28a0e730867aff81ca8
yassinebenaid <yassinebenaide3@gmail.com>
2024-09-22 10:24:13 +0100

    parser: handle unhandled redirection case

--- parser/redirection_test.go
+++ parser/redirection_test.go
@@ -3,7 +3,7 @@ package parser_test
 import "github.com/yassinebenaid/nbs/ast"
 
 var redirectionTests = []testCase{
-	{`cmd>'file.ext' arg > file>/foo/bar arg2 >"$var" arg345>xyz 3>foo.bar 45> /foo/bar 12.34>baz`, ast.Script{
+	{`cmd>'file.ext' arg > file>/foo/bar arg2 >"$var" arg345>xyz 645 >file 3>foo.bar 45> /foo/bar 12.34>baz`, ast.Script{
 		Statements: []ast.Node{
 			ast.Command{
 				Name: ast.Word("cmd"),
@@ -11,6 +11,7 @@ var redirectionTests = []testCase{
 					ast.Word("arg"),
 					ast.Word("arg2"),
 					ast.Word("arg345"),
+					ast.Word("645"),
 					ast.Word("12.34"),
 				}, Redirections: []ast.Redirection{
 					{Src: ast.FileDescriptor("1"), Method: ">", Dst: ast.Word("file.ext")},
@@ -18,6 +19,7 @@ var redirectionTests = []testCase{
 					{Src: ast.FileDescriptor("1"), Method: ">", Dst: ast.Word("/foo/bar")},
 					{Src: ast.FileDescriptor("1"), Method: ">", Dst: ast.SimpleExpansion("var")},
 					{Src: ast.FileDescriptor("1"), Method: ">", Dst: ast.Word("xyz")},
+					{Src: ast.FileDescriptor("1"), Method: ">", Dst: ast.Word("file")},
 					{Src: ast.FileDescriptor("3"), Method: ">", Dst: ast.Word("foo.bar")},
 					{Src: ast.FileDescriptor("45"), Method: ">", Dst: ast.Word("/foo/bar")},
 					{Src: ast.FileDescriptor("1"), Method: ">", Dst: ast.Word("baz")},
@@ -25,7 +27,7 @@ var redirectionTests = []testCase{
 			},
 		},
 	}},
-	{`cmd>>'file.ext' arg >> file>>/foo/bar arg2 >>"$var" arg345>>xyz 3>>foo.bar 12.34>>baz`, ast.Script{
+	{`cmd>>'file.ext' arg >> file>>/foo/bar arg2 >>"$var" arg345>>xyz 123 >>file 3>>foo.bar 12.34>>baz`, ast.Script{
 		Statements: []ast.Node{
 			ast.Command{
 				Name: ast.Word("cmd"),
@@ -33,6 +35,7 @@ var redirectionTests = []testCase{
 					ast.Word("arg"),
 					ast.Word("arg2"),
 					ast.Word("arg345"),
+					ast.Word("123"),
 					ast.Word("12.34"),
 				}, Redirections: []ast.Redirection{
 					{Src: ast.FileDescriptor("1"), Method: ">>", Dst: ast.Word("file.ext")},
@@ -40,6 +43,7 @@ var redirectionTests = []testCase{
 					{Src: ast.FileDescriptor("1"), Method: ">>", Dst: ast.Word("/foo/bar")},
 					{Src: ast.FileDescriptor("1"), Method: ">>", Dst: ast.SimpleExpansion("var")},
 					{Src: ast.FileDescriptor("1"), Method: ">>", Dst: ast.Word("xyz")},
+					{Src: ast.FileDescriptor("1"), Method: ">>", Dst: ast.Word("file")},
 					{Src: ast.FileDescriptor("3"), Method: ">>", Dst: ast.Word("foo.bar")},
 					{Src: ast.FileDescriptor("1"), Method: ">>", Dst: ast.Word("baz")},
 				},
@@ -64,7 +68,7 @@ var redirectionTests = []testCase{
 			},
 		},
 	}},
-	{`cmd>&1 arg >&2 arg>&3 arg345>&4 5>&6 12.34>& 7`, ast.Script{
+	{`cmd>&1 arg >&2 arg>&3 arg345>&4 5>&6 985 >&19 12.34>& 7 8>& 9`, ast.Script{
 		Statements: []ast.Node{
 			ast.Command{
 				Name: ast.Word("cmd"),
@@ -72,6 +76,7 @@ var redirectionTests = []testCase{
 					ast.Word("arg"),
 					ast.Word("arg"),
 					ast.Word("arg345"),
+					ast.Word("985"),
 					ast.Word("12.34"),
 				}, Redirections: []ast.Redirection{
 					{Src: ast.FileDescriptor("1"), Method: ">&", Dst: ast.FileDescriptor("1")},
@@ -79,31 +84,38 @@ var redirectionTests = []testCase{
 					{Src: ast.FileDescriptor("1"), Method: ">&", Dst: ast.FileDescriptor("3")},
 					{Src: ast.FileDescriptor("1"), Method: ">&", Dst: ast.FileDescriptor("4")},
 					{Src: ast.FileDescriptor("5"), Method: ">&", Dst: ast.FileDescriptor("6")},
+					{Src: ast.FileDescriptor("1"), Method: ">&", Dst: ast.FileDescriptor("19")},
 					{Src: ast.FileDescriptor("1"), Method: ">&", Dst: ast.FileDescriptor("7")},
+					{Src: ast.FileDescriptor("8"), Method: ">&", Dst: ast.FileDescriptor("9")},
 				},
 			},
 		},
 	}},
-	{`cmd<'file.ext' arg < file</foo/bar arg123<foo 3<bar <&3 4<&5`, ast.Script{
+	{`cmd<'file.ext' arg < file</foo/bar arg123<foo 3<bar 928 <bar 282 <&123 <&3 4<&5 6<& 7`, ast.Script{
 		Statements: []ast.Node{
 			ast.Command{
 				Name: ast.Word("cmd"),
 				Args: []ast.Node{
 					ast.Word("arg"),
 					ast.Word("arg123"),
+					ast.Word("928"),
+					ast.Word("282"),
 				}, Redirections: []ast.Redirection{
 					{Src: ast.FileDescriptor("0"), Method: "<", Dst: ast.Word("file.ext")},
 					{Src: ast.FileDescriptor("0"), Method: "<", Dst: ast.Word("file")},
 					{Src: ast.FileDescriptor("0"), Method: "<", Dst: ast.Word("/foo/bar")},
 					{Src: ast.FileDescriptor("0"), Method: "<", Dst: ast.Word("foo")},
 					{Src: ast.FileDescriptor("3"), Method: "<", Dst: ast.Word("bar")},
+					{Src: ast.FileDescriptor("0"), Method: "<", Dst: ast.Word("bar")},
+					{Src: ast.FileDescriptor("0"), Method: "<&", Dst: ast.FileDescriptor("123")},
 					{Src: ast.FileDescriptor("0"), Method: "<&", Dst: ast.FileDescriptor("3")},
 					{Src: ast.FileDescriptor("4"), Method: "<&", Dst: ast.FileDescriptor("5")},
+					{Src: ast.FileDescriptor("6"), Method: "<&", Dst: ast.FileDescriptor("7")},
 				},
 			},
 		},
 	}},
-	{`cmd<<<'foo bar' arg <<< foo<<<foo-bar arg2 <<<"$var" 3<<<foobar <<<123 4<<<123`, ast.Script{
+	{`cmd<<<'foo bar' arg <<< foo<<<foo-bar arg2 <<<"$var" 3<<<foobar <<<123 4<<<123 5<<< 	776`, ast.Script{
 		Statements: []ast.Node{
 			ast.Command{
 				Name: ast.Word("cmd"),
@@ -118,6 +130,7 @@ var redirectionTests = []testCase{
 					{Src: ast.FileDescriptor("3"), Method: "<<<", Dst: ast.Word("foobar")},
 					{Src: ast.FileDescriptor("0"), Method: "<<<", Dst: ast.Word("123")},
 					{Src: ast.FileDescriptor("4"), Method: "<<<", Dst: ast.Word("123")},
+					{Src: ast.FileDescriptor("5"), Method: "<<<", Dst: ast.Word("776")},
 				},
 			},
 		},
