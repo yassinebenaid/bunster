d69712147eade24ab8962b80ff37a89d2f9b16d4
yassinebenaid <yassinebenaide3@gmail.com>
2024-10-13 06:49:31 +0100

    parser(loops): make it works

--- parser/compounds.go
+++ parser/compounds.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/yassinebenaid/bunny/ast"
 	"github.com/yassinebenaid/bunny/token"
 )
@@ -16,5 +18,30 @@ func (p *Parser) getCompoundParser() func() ast.Node {
 
 func (p *Parser) parseWhileLoop() ast.Node {
 	var loop ast.Loop
+	p.proceed()
+	if p.curr.Type == token.BLANK {
+		p.proceed()
+	}
+
+	loop.Head = p.parseCommandList()
+
+	if p.curr.Type == token.SEMICOLON {
+		p.proceed()
+	}
+	if p.curr.Type == token.BLANK {
+		p.proceed()
+	}
+	fmt.Println(p.curr.Literal)
+	p.proceed()
+	if p.curr.Type == token.BLANK {
+		p.proceed()
+	}
+
+	loop.Body = append(loop.Body, p.parseCommandList())
+	p.proceed()
+	if p.curr.Type == token.BLANK {
+		p.proceed()
+	}
+
 	return loop
 }
