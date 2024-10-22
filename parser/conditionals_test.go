f9d1d910380228bc09a54bbb231838b155cd66ba
yassinebenaid <yassinebenaide3@gmail.com>
2024-10-22 16:49:55 +0100

    parser(conditionals): added more tests

--- parser/conditionals_test.go
+++ parser/conditionals_test.go
@@ -31,4 +31,27 @@ var conditionalsTests = []testCase{
 			},
 		},
 	}},
+	{`if
+		cmd1 | cmd2 && cmd3
+	 then
+		echo 'baz'
+	fi`, ast.Script{
+		Statements: []ast.Node{
+			ast.If{
+				Head: []ast.Node{
+					ast.BinaryConstruction{
+						Left: ast.Pipeline{
+							{Command: ast.Command{Name: ast.Word("cmd1")}},
+							{Command: ast.Command{Name: ast.Word("cmd2")}},
+						},
+						Operator: "&&",
+						Right:    ast.Command{Name: ast.Word("cmd3")},
+					},
+				},
+				Body: []ast.Node{
+					ast.Command{Name: ast.Word("echo"), Args: []ast.Node{ast.Word("baz")}},
+				},
+			},
+		},
+	}},
 }
