033b9475a1a004e00c4cd48117710a3045590348
yassinebenaid <yassinebenaide3@gmail.com>
2024-10-27 14:42:16 +0100

    parser(case): handle more cases

--- parser/case_test.go
+++ parser/case_test.go
@@ -379,4 +379,41 @@ var caseTests = []testCase{
 			},
 		},
 	}},
+	{`
+		case $foo in	bar) cmd;;esac
+		case $foo in	bar) cmd;;esac; case $foo in	bar) cmd;;esac
+	`, ast.Script{
+		Statements: []ast.Statement{
+			ast.Case{
+				Word: ast.Var("foo"),
+				Cases: []ast.CaseItem{
+					{
+						Patterns:   []ast.Expression{ast.Word("bar")},
+						Body:       []ast.Statement{ast.Command{Name: ast.Word("cmd")}},
+						Terminator: ";;",
+					},
+				},
+			},
+			ast.Case{
+				Word: ast.Var("foo"),
+				Cases: []ast.CaseItem{
+					{
+						Patterns:   []ast.Expression{ast.Word("bar")},
+						Body:       []ast.Statement{ast.Command{Name: ast.Word("cmd")}},
+						Terminator: ";;",
+					},
+				},
+			},
+			ast.Case{
+				Word: ast.Var("foo"),
+				Cases: []ast.CaseItem{
+					{
+						Patterns:   []ast.Expression{ast.Word("bar")},
+						Body:       []ast.Statement{ast.Command{Name: ast.Word("cmd")}},
+						Terminator: ";;",
+					},
+				},
+			},
+		},
+	}},
 }
