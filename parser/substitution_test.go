26187aad086ea5c34a78744f05658a4b86a25031
yassinebenaid <yassinebenaide3@gmail.com>
2024-10-29 16:34:17 +0100

    parser(subshell): clean tests

---
+++ parser/substitution_test.go
@@ -0,0 +1,113 @@
+package parser_test
+
+import "github.com/yassinebenaid/bunny/ast"
+
+var substitutionTests = []testCase{
+	{`$( cmd )`, ast.Script{
+		Statements: []ast.Statement{
+			ast.Command{
+				Name: ast.SubShell{
+					ast.Command{Name: ast.Word("cmd")},
+				},
+			},
+		},
+	}},
+	{`$( cmd; cmd )`, ast.Script{
+		Statements: []ast.Statement{
+			ast.Command{
+				Name: ast.SubShell{
+					ast.Command{Name: ast.Word("cmd")},
+					ast.Command{Name: ast.Word("cmd")},
+				},
+			},
+		},
+	}},
+	{`$( cmd; cmd; )`, ast.Script{
+		Statements: []ast.Statement{
+			ast.Command{
+				Name: ast.SubShell{
+					ast.Command{Name: ast.Word("cmd")},
+					ast.Command{Name: ast.Word("cmd")},
+				},
+			},
+		},
+	}},
+	{`$(
+		cmd
+	 	cmd
+	)`, ast.Script{
+		Statements: []ast.Statement{
+			ast.Command{
+				Name: ast.SubShell{
+					ast.Command{Name: ast.Word("cmd")},
+					ast.Command{Name: ast.Word("cmd")},
+				},
+			},
+		},
+	}},
+	{`$(cmd&cmd&)`, ast.Script{
+		Statements: []ast.Statement{
+			ast.Command{
+				Name: ast.SubShell{
+					ast.BackgroundConstruction{Statement: ast.Command{Name: ast.Word("cmd")}},
+					ast.BackgroundConstruction{Statement: ast.Command{Name: ast.Word("cmd")}},
+				},
+			},
+		},
+	}},
+	{`$(cmd1 | cmd2 && cmd3; cmd1 | cmd2 && cmd3)`, ast.Script{
+		Statements: []ast.Statement{
+			ast.Command{
+				Name: ast.SubShell{
+					ast.BinaryConstruction{
+						Left: ast.Pipeline{
+							{Command: ast.Command{Name: ast.Word("cmd1")}},
+							{Command: ast.Command{Name: ast.Word("cmd2")}},
+						},
+						Operator: "&&",
+						Right:    ast.Command{Name: ast.Word("cmd3")},
+					},
+					ast.BinaryConstruction{
+						Left: ast.Pipeline{
+							{Command: ast.Command{Name: ast.Word("cmd1")}},
+							{Command: ast.Command{Name: ast.Word("cmd2")}},
+						},
+						Operator: "&&",
+						Right:    ast.Command{Name: ast.Word("cmd3")},
+					},
+				},
+			},
+		},
+	}},
+	{`$(cmd; cmd) | $(cmd; cmd)&& $(cmd; cmd)`, ast.Script{
+		Statements: []ast.Statement{
+			ast.BinaryConstruction{
+				Left: ast.Pipeline{
+					{
+						Command: ast.Command{
+							Name: ast.SubShell{
+								ast.Command{Name: ast.Word("cmd")},
+								ast.Command{Name: ast.Word("cmd")},
+							},
+						},
+					},
+					{
+						Command: ast.Command{
+							Name: ast.SubShell{
+								ast.Command{Name: ast.Word("cmd")},
+								ast.Command{Name: ast.Word("cmd")},
+							},
+						},
+					},
+				},
+				Operator: "&&",
+				Right: ast.Command{
+					Name: ast.SubShell{
+						ast.Command{Name: ast.Word("cmd")},
+						ast.Command{Name: ast.Word("cmd")},
+					},
+				},
+			},
+		},
+	}},
+}
