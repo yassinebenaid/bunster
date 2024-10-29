280f4d0edc8df18c3b1463ac38b3bb86c4124bc3
yassinebenaid <yassinebenaide3@gmail.com>
2024-10-29 16:56:34 +0100

    parser(subshell): uncomment tests and added failing redirection tests

--- parser/grouping_test.go
+++ parser/grouping_test.go
@@ -99,124 +99,146 @@ var groupingTests = []testCase{
 		},
 	}},
 
-	// {`( cmd )`, ast.Script{
-	// 	Statements: []ast.Statement{
-	// 		ast.SubShell{
-	// 			Body:ast.Statement{
-	// 			ast.Command{Name: ast.Word("cmd")},
-	// 		},
-	// 	},
-	// }},
-	// {`( cmd; cmd )`, ast.Script{
-	// 	Statements: []ast.Statement{
-	// 		ast.SubShell{
-	// 			Body:ast.Statement{
-	// 			ast.Command{Name: ast.Word("cmd")},
-	// 			ast.Command{Name: ast.Word("cmd")},
-	// 		},
-	// 	},
-	// }},
-	// {`( cmd; cmd; )`, ast.Script{
-	// 	Statements: []ast.Statement{
-	// 		ast.SubShell{
-	// 			Body:ast.Statement{
-	// 			ast.Command{Name: ast.Word("cmd")},
-	// 			ast.Command{Name: ast.Word("cmd")},
-	// 		},
-	// 	},
-	// }},
+	{`( cmd )`, ast.Script{
+		Statements: []ast.Statement{
+			ast.SubShell{
+				Body: []ast.Statement{
+					ast.Command{Name: ast.Word("cmd")},
+				},
+			},
+		},
+	}},
+	{`( cmd; cmd )`, ast.Script{
+		Statements: []ast.Statement{
+			ast.SubShell{
+				Body: []ast.Statement{
+					ast.Command{Name: ast.Word("cmd")},
+					ast.Command{Name: ast.Word("cmd")},
+				},
+			},
+		},
+	}},
+	{`( cmd; cmd; )`, ast.Script{
+		Statements: []ast.Statement{
+			ast.SubShell{
+				Body: []ast.Statement{
+					ast.Command{Name: ast.Word("cmd")},
+					ast.Command{Name: ast.Word("cmd")},
+				},
+			},
+		},
+	}},
 
-	// {`(cmd;cmd)`, ast.Script{
-	// 	Statements: []ast.Statement{
-	// 		ast.SubShell{
-	// 			Body:ast.Statement{
-	// 			ast.Command{Name: ast.Word("cmd")},
-	// 			ast.Command{Name: ast.Word("cmd")},
-	// 		},
-	// 	},
-	// }},
+	{`(cmd;cmd)`, ast.Script{
+		Statements: []ast.Statement{
+			ast.SubShell{
+				Body: []ast.Statement{
+					ast.Command{Name: ast.Word("cmd")},
+					ast.Command{Name: ast.Word("cmd")},
+				},
+			},
+		},
+	}},
 
-	// {`(
-	// 	cmd
-	//  	cmd
-	// )`, ast.Script{
-	// 	Statements: []ast.Statement{
-	// 		ast.SubShell{
-	// 			Body:ast.Statement{
-	// 			ast.Command{Name: ast.Word("cmd")},
-	// 			ast.Command{Name: ast.Word("cmd")},
-	// 			},
-	// 		},
-	// 	},
-	// }},
-	// {`(cmd&cmd&)`, ast.Script{
-	// 	Statements: []ast.Statement{
-	// 		ast.SubShell{
-	// 			Body:ast.Statement{
-	// 			ast.BackgroundConstruction{Statement: ast.Command{Name: ast.Word("cmd")}},
-	// 			ast.BackgroundConstruction{Statement: ast.Command{Name: ast.Word("cmd")}},
-	// 			},
-	// 		},
-	// 	},
-	// }},
-	// {`(cmd1 | cmd2 && cmd3; cmd1 | cmd2 && cmd3)`, ast.Script{
-	// 	Statements: []ast.Statement{
-	// 		ast.SubShell{
-	// 			Body:ast.Statement{
-	// 			ast.BinaryConstruction{
-	// 				Left: ast.Pipeline{
-	// 					{Command: ast.Command{Name: ast.Word("cmd1")}},
-	// 					{Command: ast.Command{Name: ast.Word("cmd2")}},
-	// 				},
-	// 				Operator: "&&",
-	// 				Right:    ast.Command{Name: ast.Word("cmd3")},
-	// 			},
-	// 			ast.BinaryConstruction{
-	// 				Left: ast.Pipeline{
-	// 					{Command: ast.Command{Name: ast.Word("cmd1")}},
-	// 					{Command: ast.Command{Name: ast.Word("cmd2")}},
-	// 				},
-	// 				Operator: "&&",
-	// 				Right:    ast.Command{Name: ast.Word("cmd3")},
-	// 			},
-	// 		},
-	// 	},
-	// }},
+	{`(
+		cmd
+	 	cmd
+	)`, ast.Script{
+		Statements: []ast.Statement{
+			ast.SubShell{
+				Body: []ast.Statement{
+					ast.Command{Name: ast.Word("cmd")},
+					ast.Command{Name: ast.Word("cmd")},
+				},
+			},
+		},
+	}},
+	{`(cmd&cmd&)`, ast.Script{
+		Statements: []ast.Statement{
+			ast.SubShell{
+				Body: []ast.Statement{
+					ast.BackgroundConstruction{Statement: ast.Command{Name: ast.Word("cmd")}},
+					ast.BackgroundConstruction{Statement: ast.Command{Name: ast.Word("cmd")}},
+				},
+			},
+		},
+	}},
+	{`(cmd1 | cmd2 && cmd3; cmd1 | cmd2 && cmd3)`, ast.Script{
+		Statements: []ast.Statement{
+			ast.SubShell{
+				Body: []ast.Statement{
+					ast.BinaryConstruction{
+						Left: ast.Pipeline{
+							{Command: ast.Command{Name: ast.Word("cmd1")}},
+							{Command: ast.Command{Name: ast.Word("cmd2")}},
+						},
+						Operator: "&&",
+						Right:    ast.Command{Name: ast.Word("cmd3")},
+					},
+					ast.BinaryConstruction{
+						Left: ast.Pipeline{
+							{Command: ast.Command{Name: ast.Word("cmd1")}},
+							{Command: ast.Command{Name: ast.Word("cmd2")}},
+						},
+						Operator: "&&",
+						Right:    ast.Command{Name: ast.Word("cmd3")},
+					},
+				},
+			},
+		},
+	}},
 
-	// {`(cmd; cmd) | (cmd; cmd)&& (cmd; cmd)`, ast.Script{
-	// 	Statements: []ast.Statement{
-	// 		ast.BinaryConstruction{
-	// 			Left: ast.Pipeline{
-	// 				{Command: ast.SubShell{
-	// 					Body:ast.Statement{
-	// 					ast.Command{Name: ast.Word("cmd")},
-	// 					ast.Command{Name: ast.Word("cmd")},
-	// 				}},
-	// 				{Command: ast.SubShell{
-	// 					Body:ast.Statement{
-	// 					ast.Command{Name: ast.Word("cmd")},
-	// 					ast.Command{Name: ast.Word("cmd")},
-	// 				}},
-	// 			},
-	// 			Operator: "&&",
-	// 			Right: ast.SubShell{
-	// 				Body:ast.Statement{
-	// 				ast.Command{Name: ast.Word("cmd")},
-	// 				ast.Command{Name: ast.Word("cmd")},
-	// 			},
-	// 		},
-	// 	},
-	// }},
-	// {`(cmd) >output.txt <input.txt 2>error.txt >&3 \
-	// 	 	>>output.txt <<<input.txt 2>>error.txt &>all.txt &>>all.txt <&4 5<&6`, ast.Script{
-	// 	Statements: []ast.Statement{
-	// 		ast.SubShell{
-	// 			Body:ast.Statement{
-	// 			ast.Command{Name: ast.Word("cmd")},
-	// 			ast.Command{Name: ast.Word("cmd")},
-	// 		},
-	// 	},
-	// }},
+	{`(cmd; cmd) | (cmd; cmd)&& (cmd; cmd)`, ast.Script{
+		Statements: []ast.Statement{
+			ast.BinaryConstruction{
+				Left: ast.Pipeline{
+					{Command: ast.SubShell{
+						Body: []ast.Statement{
+							ast.Command{Name: ast.Word("cmd")},
+							ast.Command{Name: ast.Word("cmd")},
+						},
+					}},
+					{Command: ast.SubShell{
+						Body: []ast.Statement{
+							ast.Command{Name: ast.Word("cmd")},
+							ast.Command{Name: ast.Word("cmd")},
+						},
+					}},
+				},
+				Operator: "&&",
+				Right: ast.SubShell{
+					Body: []ast.Statement{
+						ast.Command{Name: ast.Word("cmd")},
+						ast.Command{Name: ast.Word("cmd")},
+					},
+				},
+			},
+		},
+	}},
+	{`(cmd) >output.txt <input.txt 2>error.txt >&3 \
+		 	>>output.txt <<<input.txt 2>>error.txt &>all.txt &>>all.txt <&4 5<&6`, ast.Script{
+		Statements: []ast.Statement{
+			ast.SubShell{
+				Body: []ast.Statement{
+					ast.Command{Name: ast.Word("cmd")},
+					ast.Command{Name: ast.Word("cmd")},
+				},
+				Redirections: []ast.Redirection{
+					{Src: "1", Method: ">", Dst: ast.Word("output.txt")},
+					{Src: "0", Method: "<", Dst: ast.Word("input.txt")},
+					{Src: "2", Method: ">", Dst: ast.Word("error.txt")},
+					{Src: "1", Method: ">&", Dst: ast.Word("3")},
+					{Src: "1", Method: ">>", Dst: ast.Word("output.txt")},
+					{Src: "0", Method: "<<<", Dst: ast.Word("input.txt")},
+					{Src: "2", Method: ">>", Dst: ast.Word("error.txt")},
+					{Method: "&>", Dst: ast.Word("all.txt")},
+					{Method: "&>>", Dst: ast.Word("all.txt")},
+					{Src: "0", Method: "<&", Dst: ast.Word("4")},
+					{Src: "5", Method: "<&", Dst: ast.Word("6")},
+				},
+			},
+		},
+	}},
 }
 
 var groupingErrorHandlingCases = []errorHandlingTestCase{
