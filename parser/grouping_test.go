e20316809375e57fb760fbfe7b2d1469c78cbf0b
yassinebenaid <yassinebenaide3@gmail.com>
2024-10-29 16:24:21 +0100

    parser(subshell): added more test cases

--- parser/grouping_test.go
+++ parser/grouping_test.go
@@ -248,6 +248,30 @@ var groupingTests = []testCase{
 			},
 		},
 	}},
+	{`$(cmd1 | cmd2 && cmd3; cmd1 | cmd2 && cmd3)`, ast.Script{
+		Statements: []ast.Statement{
+			ast.Command{
+				Name: ast.SubShell{
+					ast.BinaryConstruction{
+						Left: ast.Pipeline{
+							{Command: ast.Command{Name: ast.Word("cmd1")}},
+							{Command: ast.Command{Name: ast.Word("cmd2")}},
+						},
+						Operator: "&&",
+						Right:    ast.Command{Name: ast.Word("cmd3")},
+					},
+					ast.BinaryConstruction{
+						Left: ast.Pipeline{
+							{Command: ast.Command{Name: ast.Word("cmd1")}},
+							{Command: ast.Command{Name: ast.Word("cmd2")}},
+						},
+						Operator: "&&",
+						Right:    ast.Command{Name: ast.Word("cmd3")},
+					},
+				},
+			},
+		},
+	}},
 }
 
 var groupingErrorHandlingCases = []errorHandlingTestCase{
