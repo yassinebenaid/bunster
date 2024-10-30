5428629a53ea34484be3a4325bad0828eb68c162
yassinebenaid <yassinebenaide3@gmail.com>
2024-10-30 16:01:54 +0100

    process substituion: make it work

--- parser/substitutions.go
+++ parser/substitutions.go
@@ -36,3 +36,42 @@ func (p *Parser) parseCommandSubstitution() ast.Expression {
 
 	return cmds
 }
+
+func (p *Parser) parseProcessSubstitution() ast.Expression {
+	tok := p.curr.Literal
+	var process ast.ProcessSubstitution
+
+	process.Direction = '>'
+	if tok == "<(" {
+		process.Direction = '<'
+	}
+
+	p.proceed()
+	for p.curr.Type == token.BLANK || p.curr.Type == token.NEWLINE {
+		p.proceed()
+	}
+
+	for p.curr.Type != token.RIGHT_PAREN && p.curr.Type != token.EOF {
+		cmdList := p.parseCommandList()
+		if cmdList == nil {
+			return nil
+		}
+		process.Body = append(process.Body, cmdList)
+		if p.curr.Type == token.SEMICOLON || p.curr.Type == token.AMPERSAND {
+			p.proceed()
+		}
+		for p.curr.Type == token.BLANK || p.curr.Type == token.NEWLINE {
+			p.proceed()
+		}
+	}
+
+	if len(process.Body) == 0 {
+		p.error("expeceted a command list after `%s`", tok)
+	}
+
+	if p.curr.Type != token.RIGHT_PAREN {
+		p.error("unexpected end of file, expeceted `)`")
+	}
+
+	return process
+}
