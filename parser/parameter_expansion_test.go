38cbffa1292af934796e01b323825285fb6acfeb
yassinebenaid <yassinebenaide3@gmail.com>
2024-10-31 15:31:43 +0100

    parameter expansion: handle "${var}"

--- parser/parameter_expansion_test.go
+++ parser/parameter_expansion_test.go
@@ -3,7 +3,12 @@ package parser_test
 import "github.com/yassinebenaid/bunny/ast"
 
 var parameterExpansionCases = []testCase{
-	{`${var}`, ast.Script{Statements: []ast.Statement{
-		ast.Command{Name: ast.Var("var")},
+	{`cmd ${var} `, ast.Script{Statements: []ast.Statement{
+		ast.Command{
+			Name: ast.Word("cmd"),
+			Args: []ast.Expression{
+				ast.Var("var"),
+			},
+		},
 	}}},
 }
