90371baf0902de1c922d2f06bee5918fee3f5105
yassinebenaid <yassinebenaide3@gmail.com>
2024-11-02 15:44:18 +0100

    parameter expansion: cleanup

--- parser/parameter_expansion_test.go
+++ parser/parameter_expansion_test.go
@@ -12,26 +12,23 @@ var parameterExpansionCases = []testCase{
 			},
 		},
 	}}},
-	{
-		`cmd ${var-default} ${var-${default}} ${var- $foo bar baz | & ; 2> < }`,
-		ast.Script{Statements: []ast.Statement{
-			ast.Command{
-				Name: ast.Word("cmd"),
-				Args: []ast.Expression{
-					ast.VarOrDefault{Name: "var", Default: ast.Word("default")},
-					ast.VarOrDefault{Name: "var", Default: ast.Var("default")},
-					ast.VarOrDefault{
-						Name: "var",
-						Default: ast.Concatination{
-							Nodes: []ast.Expression{
-								ast.Word(" "),
-								ast.Var("foo"),
-								ast.Word(" bar baz | & ; 2> < "),
-							},
+	{`cmd ${var-default} ${var-${default}} ${var- $foo bar baz | & ; 2> < }`, ast.Script{Statements: []ast.Statement{
+		ast.Command{
+			Name: ast.Word("cmd"),
+			Args: []ast.Expression{
+				ast.VarOrDefault{Name: "var", Default: ast.Word("default")},
+				ast.VarOrDefault{Name: "var", Default: ast.Var("default")},
+				ast.VarOrDefault{
+					Name: "var",
+					Default: ast.Concatination{
+						Nodes: []ast.Expression{
+							ast.Word(" "),
+							ast.Var("foo"),
+							ast.Word(" bar baz | & ; 2> < "),
 						},
 					},
 				},
 			},
-		}},
-	},
+		},
+	}}},
 }
