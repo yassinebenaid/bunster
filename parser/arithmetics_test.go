0a01e699077c01558e5707571e6bbb29a3811073
yassinebenaid <yassinebenaide3@gmail.com>
2024-11-10 11:08:25 +0100

    arithmetics: wip

--- parser/arithmetics_test.go
+++ parser/arithmetics_test.go
@@ -453,6 +453,7 @@ var arithmeticsPrecedenceTests = []struct {
 	29: {`$(( a == b <= c, a == b >= c, a == b < c, a == b > c, a != b <= c, a != b >= c, a != b < c, a != b > c ))`,
 		`(a == (b <= c)), (a == (b >= c)), (a == (b < c)), (a == (b > c)), (a != (b <= c)), (a != (b >= c)), (a != (b < c)), (a != (b > c))`},
 	30: {`$(( a & b == c, a & b != c))`, `(a & (b == c)), (a & (b != c))`},
+	31: {`$(( a ^ b & c))`, `(a ^ (b & c))`},
 }
 
 func TestArithmeticsPrecedence(t *testing.T) {
