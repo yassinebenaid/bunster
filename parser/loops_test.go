1a9327d2f2199c0b60da8e63cfbb67141e1c58b6
yassinebenaid <yassinebenaide3@gmail.com>
2024-11-11 16:18:21 +0100

    loops: added more tests

--- parser/loops_test.go
+++ parser/loops_test.go
@@ -953,6 +953,12 @@ var loopsTests = []testCase{
 			Body: []ast.Statement{ast.Command{Name: ast.Word("cmd")}},
 		},
 	}},
+	{`for ((;;)) do cmd;done`, ast.Script{
+		ast.For{
+			Head: ast.ForHead{},
+			Body: []ast.Statement{ast.Command{Name: ast.Word("cmd")}},
+		},
+	}},
 }
 
 var loopsErrorHandlingCases = []errorHandlingTestCase{
