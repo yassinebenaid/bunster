a322929534ff8f30e0475b53f002ac28329895ed
yassinebenaid <yassinebenaide3@gmail.com>
2024-11-17 15:16:29 +0100

    functions: handle functions parse errors

--- parser/functions_test.go
+++ parser/functions_test.go
@@ -65,4 +65,5 @@ var functionsErrorHandlingCases = []errorHandlingTestCase{
 	{`foo () simple_command`, "syntax error: bad function definition, invalid token `simple_command`. (line: 1, column: 8)"},
 	{`$foo () {cmd;}`, "syntax error: invalid function name was supplied. (line: 1, column: 6)"},
 	{`cmd arg (`, "syntax error: token `(` cannot be placed here. (line: 1, column: 9)"},
+	{`cmd arg )`, "syntax error: token `)` cannot be placed here. (line: 1, column: 9)"},
 }
