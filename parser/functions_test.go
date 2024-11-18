449dcf839d4d974f7223490ab5c42964dcecefbf
yassinebenaid <yassinebenaide3@gmail.com>
2024-11-18 12:11:43 +0100

    functions("function" keyword): added failing test

--- parser/functions_test.go
+++ parser/functions_test.go
@@ -71,6 +71,16 @@ var functionsTests = []testCase{
 			},
 		},
 	}},
+	{`function foo(){ cmd; }`, ast.Script{
+		ast.Function{
+			Name: "foo",
+			Command: ast.Group{
+				Body: []ast.Statement{
+					ast.Command{Name: ast.Word("cmd")},
+				},
+			},
+		},
+	}},
 }
 
 var functionsErrorHandlingCases = []errorHandlingTestCase{
