268eb2ecdbc071525e880e2666d3da9be9583ea3
yassinebenaid <yassinebenaide3@gmail.com>
2024-11-18 15:56:14 +0100

    parameter assignement: added more test cases

--- parser/parameter_assignment_test.go
+++ parser/parameter_assignment_test.go
@@ -3,10 +3,11 @@ package parser_test
 import "github.com/yassinebenaid/bunny/ast"
 
 var parameterAssignmentTests = []testCase{
-	{`var=value var2=value2`, ast.Script{
+	{`var=value var2='value2'    var3="value3"`, ast.Script{
 		ast.ParameterAssignement{
 			ast.Assignement{Name: "var", Value: ast.Word("value")},
 			ast.Assignement{Name: "var2", Value: ast.Word("value2")},
+			ast.Assignement{Name: "var3", Value: ast.Word("value3")},
 		},
 	}},
 	{`var=$var var=${var}`, ast.Script{
