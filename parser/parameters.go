510e91bbdcb69ec611b2b686bb8da43ed7f82086
yassinebenaid <yassinebenaide3@gmail.com>
2024-11-18 15:43:31 +0100

    parameter assignement: make it work

--- parser/parameters.go
+++ parser/parameters.go
@@ -2,8 +2,22 @@ package parser
 
 import (
 	"github.com/yassinebenaid/bunny/ast"
+	"github.com/yassinebenaid/bunny/token"
 )
 
 func (p *Parser) parseAssignement() ast.ParameterAssignement {
-	return nil
+	var assigns ast.ParameterAssignement
+
+	for {
+		if !(p.curr.Type == token.WORD && p.next.Type == token.ASSIGN) {
+			break
+		}
+		assignment := ast.Assignement{Name: p.curr.Literal}
+		p.proceed()
+		p.proceed()
+		assignment.Value = p.parseExpression()
+		assigns = append(assigns, assignment)
+	}
+
+	return assigns
 }
