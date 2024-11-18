07463f6a50578d42ef4a781317923c42b0318a14
yassinebenaid <yassinebenaide3@gmail.com>
2024-11-18 12:22:03 +0100

    functions("function" keyword): handle more cases

--- parser/functions_test.go
+++ parser/functions_test.go
@@ -81,6 +81,62 @@ var functionsTests = []testCase{
 			},
 		},
 	}},
+	{`function foo-bar-baz () { cmd; }`, ast.Script{
+		ast.Function{
+			Name: "foo-bar-baz",
+			Command: ast.Group{
+				Body: []ast.Statement{
+					ast.Command{Name: ast.Word("cmd")},
+				},
+			},
+		},
+	}},
+	{`function "foo-bar-baz" () { cmd; }`, ast.Script{
+		ast.Function{
+			Name: "foo-bar-baz",
+			Command: ast.Group{
+				Body: []ast.Statement{
+					ast.Command{Name: ast.Word("cmd")},
+				},
+			},
+		},
+	}},
+	{`function "foo"-"bar"-'baz' () { cmd; } `, ast.Script{
+		ast.Function{
+			Name: "foo-bar-baz",
+			Command: ast.Group{
+				Body: []ast.Statement{
+					ast.Command{Name: ast.Word("cmd")},
+				},
+			},
+		},
+	}},
+	{`function foo () { cmd; } >output.txt`, ast.Script{
+		ast.Function{
+			Name: "foo",
+			Command: ast.Group{
+				Body: []ast.Statement{
+					ast.Command{Name: ast.Word("cmd")},
+				},
+				Redirections: []ast.Redirection{
+					{Src: "1", Method: ">", Dst: ast.Word("output.txt")},
+				},
+			},
+		},
+	}},
+	{`function foo (  )
+	 {
+		cmd
+	}`, ast.Script{
+		ast.Function{
+			Name: "foo",
+			Command: ast.Group{
+				Body: []ast.Statement{
+					ast.Command{Name: ast.Word("cmd")},
+				},
+			},
+		},
+	}},
 }
 
 var functionsErrorHandlingCases = []errorHandlingTestCase{
