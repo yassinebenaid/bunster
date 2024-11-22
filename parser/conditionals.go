9943f2139932d0718c8ef2b66feba928197b3da0
yassinebenaid <yassinebenaide3@gmail.com>
2024-11-22 14:46:15 +0100

    conditionals: cover '!='

--- parser/conditionals.go
+++ parser/conditionals.go
@@ -87,7 +87,7 @@ func (p *Parser) parseUnaryConditional() ast.Expression {
 
 func (p *Parser) parseConditionalBinaryOperator() string {
 	switch p.curr.Type {
-	case token.ASSIGN, token.EQ:
+	case token.ASSIGN, token.EQ, token.NOT_EQ:
 		if p.next.Type != token.BLANK {
 			break
 		}
