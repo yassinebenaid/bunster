9511b34f05087f98a776deb38dbe9a7fea31c7d9
yassinebenaid <yassinebenaide3@gmail.com>
2024-11-23 15:08:30 +0100

    conditionals: handle more critical cases in [[...]]

--- parser/conditionals_test.go
+++ parser/conditionals_test.go
@@ -189,11 +189,17 @@ var conditionalsTests = []testCase{
 	}},
 	{`
 		[[ !file1 && !file2 ]]
+		[[ ! -a file1 && ! -b file2 ]]
 	`, ast.Script{
 		ast.Test{Expr: ast.BinaryConditional{
 			Left:     ast.Negation{Operand: ast.Word("file1")},
 			Operator: "&&",
 			Right:    ast.Negation{Operand: ast.Word("file2")},
+		}},
+		ast.Test{Expr: ast.BinaryConditional{
+			Left:     ast.Negation{Operand: ast.UnaryConditional{Operator: "-a", Operand: ast.Word("file1")}},
+			Operator: "&&",
+			Right:    ast.Negation{Operand: ast.UnaryConditional{Operator: "-b", Operand: ast.Word("file2")}},
 		}},
 	}},
 }
