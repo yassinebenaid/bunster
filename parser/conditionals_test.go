718b506f16d318aee0b1e75fc7c4d3ae68c771cb
yassinebenaid <yassinebenaide3@gmail.com>
2024-11-23 14:53:51 +0100

    conditionals: handle more critical cases in [[...]]

--- parser/conditionals_test.go
+++ parser/conditionals_test.go
@@ -155,6 +155,7 @@ var conditionalsTests = []testCase{
 	{`
 		[[ file1 -ef file2 && file1 -ef file2 ]]
 		[[ file1 -ef file2 || file1 -ef file2 ]]
+		[[ file1 -ef file2 && file1 -ef file2 || file1 -ef file2 ]]
 	`, ast.Script{
 		ast.Test{Expr: ast.BinaryConditional{
 			Left:     ast.BinaryConditional{Left: ast.Word("file1"), Operator: "-ef", Right: ast.Word("file2")},
@@ -163,6 +164,15 @@ var conditionalsTests = []testCase{
 		}},
 		ast.Test{Expr: ast.BinaryConditional{
 			Left:     ast.BinaryConditional{Left: ast.Word("file1"), Operator: "-ef", Right: ast.Word("file2")},
+			Operator: "||",
+			Right:    ast.BinaryConditional{Left: ast.Word("file1"), Operator: "-ef", Right: ast.Word("file2")},
+		}},
+		ast.Test{Expr: ast.BinaryConditional{
+			Left: ast.BinaryConditional{
+				Left:     ast.BinaryConditional{Left: ast.Word("file1"), Operator: "-ef", Right: ast.Word("file2")},
+				Operator: "&&",
+				Right:    ast.BinaryConditional{Left: ast.Word("file1"), Operator: "-ef", Right: ast.Word("file2")},
+			},
 			Operator: "||",
 			Right:    ast.BinaryConditional{Left: ast.Word("file1"), Operator: "-ef", Right: ast.Word("file2")},
 		}},
