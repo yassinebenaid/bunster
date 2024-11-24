34f3f6380bbf7997c41a4c66f3756c8281478968
yassinebenaid <yassinebenaide3@gmail.com>
2024-11-24 12:29:59 +0100

    conditionals: handle more parse errors

--- parser/conditionals_test.go
+++ parser/conditionals_test.go
@@ -730,6 +730,7 @@ var conditionalsTests = []testCase{
 
 var conditionalsErrorHandlingCases = []errorHandlingTestCase{
 	{`[[`, "syntax error: bad conditional expression, unexpected token `end of file`. (line: 1, column: 3)"},
+	{`[[ &`, "syntax error: bad conditional expression, unexpected token `&`. (line: 1, column: 4)"},
 	{`[[]]`, "syntax error: expected a conditional expression before `]]`. (line: 1, column: 3)"},
 	{`[[  ]]`, "syntax error: expected a conditional expression before `]]`. (line: 1, column: 5)"},
 	{`[[  file `, "syntax error: expected `]]` to close conditional expression, found `end of file`. (line: 1, column: 10)"},
