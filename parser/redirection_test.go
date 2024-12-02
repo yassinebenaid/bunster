60b7eb4e4da9d5fd448b36e14277ef25649e65be
yassinebenaid <yassinebenaide3@gmail.com>
2024-12-02 22:10:03 +0100

    added more tests

--- parser/redirection_test.go
+++ parser/redirection_test.go
@@ -188,6 +188,21 @@ var redirectionTests = []testCase{
 			},
 		},
 	}},
+	{`cmd<&-2<&->&-2>&-<&5-6<&5->&5-6>&5-`, ast.Script{
+		ast.Command{
+			Name: ast.Word("cmd"),
+			Redirections: []ast.Redirection{
+				{Src: "0", Method: "<&", Close: true},
+				{Src: "2", Method: "<&", Close: true},
+				{Src: "1", Method: ">&", Close: true},
+				{Src: "2", Method: ">&", Close: true},
+				{Src: "0", Method: "<&", Dst: ast.Number("5"), Close: true},
+				{Src: "6", Method: "<&", Dst: ast.Number("5"), Close: true},
+				{Src: "1", Method: ">&", Dst: ast.Number("5"), Close: true},
+				{Src: "6", Method: ">&", Dst: ast.Number("5"), Close: true},
+			},
+		},
+	}},
 }
 
 var redirectionErrorHandlingCases = []errorHandlingTestCase{
