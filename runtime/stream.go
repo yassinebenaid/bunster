9ef6f326542445fd136df98b8ff7f73da754961f
yassinebenaid <yassinebenaide3@gmail.com>
2024-12-07 14:55:30 +0100

    bug fix: when duplicating file descritpors, we syscall the kernel to copy out the original fd too

--- runtime/stream.go
+++ runtime/stream.go
@@ -91,8 +91,11 @@ func (fdt FileDescriptorTable) Duplicate(oldfd, newfd string) error {
 	if stream, ok := fdt[newfd]; !ok {
 		return fmt.Errorf("trying to duplicate bad file descriptor: %s", newfd)
 	} else {
-		// TODO: reopen stream here
-		fdt[oldfd] = stream
+		dupFd, err := syscall.Dup(int(stream.Fd()))
+		if err != nil {
+			return fmt.Errorf("failed to duplicate file descriptor '%s', %w", newfd, err)
+		}
+		fdt[oldfd] = os.NewFile(uintptr(dupFd), fmt.Sprintf("/dev/fd/%d", dupFd))
 		return nil
 	}
 }
