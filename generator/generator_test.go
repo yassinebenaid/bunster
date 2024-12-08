b6dd9b420f2f556c174485c40c4c8540bc6a5820
yassinebenaid <yassinebenaide3@gmail.com>
2024-12-08 15:29:34 +0100

    added our first test

--- generator/generator_test.go
+++ generator/generator_test.go
@@ -1,16 +1,11 @@
 package generator_test
 
 import (
-	"fmt"
 	"os"
-	"reflect"
-	"strings"
+	"path/filepath"
 	"testing"
 
-	"github.com/yassinebenaid/bunster/generator"
-	"github.com/yassinebenaid/bunster/ir"
-	"github.com/yassinebenaid/bunster/lexer"
-	"github.com/yassinebenaid/bunster/parser"
+	"github.com/yassinebenaid/bunster/pkg/tst"
 	"github.com/yassinebenaid/godump"
 )
 
@@ -19,44 +14,23 @@ var dump = (&godump.Dumper{
 	ShowPrimitiveNamedTypes: true,
 }).Sprintln
 
-type testCase struct {
-	input    string
-	expected ir.Program
-}
-
-var testCases = []struct {
-	label string
-	cases []testCase
-}{
-	{"Simple Commands", []testCase{}},
-}
-
 func TestGenerator(t *testing.T) {
-	tgroup, tcase := os.Getenv("TEST_GROUP"), os.Getenv("TEST_CASE")
+	testFiles, err := filepath.Glob("./tests/*.tst")
+	if err != nil {
+		t.Fatalf("Failed to `Glob` test files, %v", err)
+	}
 
-	for _, group := range testCases {
-		if tgroup != "" && !strings.Contains(strings.ToLower(group.label), tgroup) {
-			continue
+	for _, testFile := range testFiles {
+		file, err := os.Open(testFile)
+		if err != nil {
+			t.Fatalf("Failed to open test file %s, %v", testFile, err)
 		}
 
-		for i, tc := range group.cases {
-			if tcase != "" && fmt.Sprint(i) != tcase {
-				continue
-			}
-
-			script, err := parser.Parse(
-				lexer.New([]byte(tc.input)),
-			)
-
-			if err != nil {
-				t.Fatalf("\nGroup: %sCase: %sInput: %s\nUnexpected Error: %s\n", dump(group.label), dump(i), dump(tc.input), dump(err.Error()))
-			}
-
-			program := generator.Generate(script)
-
-			if !reflect.DeepEqual(program, tc.expected) {
-				t.Fatalf("\nGroup: %sCase: %sInput: %s\nWant:\n%s\nGot:\n%s", dump(group.label), dump(i), dump(tc.input), dump(tc.expected), dump(program))
-			}
+		cases, err := tst.Parse(file)
+		if err != nil {
+			t.Fatalf("%v, (%s)", err, testFile)
 		}
+
+		_ = cases
 	}
 }
---
+++ generator/tests/0-simple-commands.tst
@@ -0,0 +1,13 @@
+Test: Simple commands
+------------
+
+--------input--------
+# Nothing but this comment
+# we need to make sure empty shell scripts produce empty go programs
+
+
+--------output--------
+module main
+
+func Main(){
+}
