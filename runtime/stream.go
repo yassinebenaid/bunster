d35720e41a7cc44cc7cd265c2194939260d36174
yassinebenaid <yassinebenaide3@gmail.com>
2024-12-26 07:31:50 +0100

    fix naming issue

--- runtime/stream.go
+++ runtime/stream.go
@@ -56,40 +56,40 @@ type StreamManager struct {
 	cleaners []func() error
 }
 
-func (fdt *StreamManager) OpenStream(name string, flag int) (Stream, error) {
+func (sm *StreamManager) OpenStream(name string, flag int) (Stream, error) {
 	switch name {
 	case "/dev/stdin":
-		return fdt.Get("0"), nil
+		return sm.Get("0"), nil
 	case "/dev/stdout":
-		return fdt.Get("1"), nil
+		return sm.Get("1"), nil
 	case "/dev/stderr":
-		return fdt.Get("2"), nil
+		return sm.Get("2"), nil
 	default:
 		return os.OpenFile(name, flag, 0644)
 	}
 }
 
-func (fdt *StreamManager) Add(fd string, stream Stream) {
+func (sm *StreamManager) Add(fd string, stream Stream) {
 	// If this stream is already open, we need to close it. otherwise, Its handler will be lost and leak.
 	// This is related to pipelines in particular. when instantiating a new pipeline, we add its ends to the FDT. but if
 	// a redirection happened afterwards, it will cause the pipline handler to be lost and kept open.
-	if fdt.mappings[fd] != nil {
-		fdt.cleaners = append(fdt.cleaners, fdt.mappings[fd].Close)
+	if sm.mappings[fd] != nil {
+		sm.cleaners = append(sm.cleaners, sm.mappings[fd].Close)
 	}
 
-	fdt.mappings[fd] = stream
+	sm.mappings[fd] = stream
 }
 
-func (fdt *StreamManager) Get(fd string) Stream {
-	stream, ok := fdt.mappings[fd]
-	if !ok && fdt.parent != nil {
-		return fdt.parent.Get(fd)
+func (sm *StreamManager) Get(fd string) Stream {
+	stream, ok := sm.mappings[fd]
+	if !ok && sm.parent != nil {
+		return sm.parent.Get(fd)
 	}
 	return stream
 }
 
-func (fdt *StreamManager) Duplicate(newfd, oldfd string) error {
-	if stream := fdt.Get(oldfd); stream == nil {
+func (sm *StreamManager) Duplicate(newfd, oldfd string) error {
+	if stream := sm.Get(oldfd); stream == nil {
 		return fmt.Errorf("trying to duplicate bad file descriptor: %s", oldfd)
 	} else {
 		// when trying to duplicate a file descriptor to it self (eg: 3>&3 ), we just return.
@@ -99,8 +99,8 @@ func (fdt *StreamManager) Duplicate(newfd, oldfd string) error {
 
 		// If the new fd is already open, we need to close it. otherwise, Its handler will be lost and leak. and remain open forever.
 		// for example: "3<file.txt 3<&0", we don't explicitly close 3. Thus, it is going to remain open forever, unless we implicitly close it here.
-		if fdt.mappings[newfd] != nil {
-			fdt.cleaners = append(fdt.cleaners, fdt.mappings[newfd].Close)
+		if sm.mappings[newfd] != nil {
+			sm.cleaners = append(sm.cleaners, sm.mappings[newfd].Close)
 		}
 
 		switch stream := stream.(type) {
@@ -110,13 +110,13 @@ func (fdt *StreamManager) Duplicate(newfd, oldfd string) error {
 			if err != nil {
 				return fmt.Errorf("failed to duplicate file descriptor '%s', %w", oldfd, err)
 			}
-			fdt.mappings[newfd] = &stringStream{buf: newbuf}
+			sm.mappings[newfd] = &stringStream{buf: newbuf}
 		case *os.File:
 			dupFd, err := syscall.Dup(int(stream.Fd()))
 			if err != nil {
 				return fmt.Errorf("failed to duplicate file descriptor '%s', %w", oldfd, err)
 			}
-			fdt.mappings[newfd] = os.NewFile(uintptr(dupFd), stream.Name())
+			sm.mappings[newfd] = os.NewFile(uintptr(dupFd), stream.Name())
 		default:
 			panic(fmt.Sprintf("failed to clone (%s), unhandled stream type: %T", oldfd, stream))
 		}
@@ -125,26 +125,26 @@ func (fdt *StreamManager) Duplicate(newfd, oldfd string) error {
 	}
 }
 
-func (fdt *StreamManager) Close(fd string) error {
-	if stream, ok := fdt.mappings[fd]; !ok {
+func (sm *StreamManager) Close(fd string) error {
+	if stream, ok := sm.mappings[fd]; !ok {
 		return fmt.Errorf("trying to close bad file descriptor: %s", fd)
 	} else {
 		return stream.Close()
 	}
 }
 
-func (fdt *StreamManager) Destroy() {
-	for _, cleanup := range fdt.cleaners {
+func (sm *StreamManager) Destroy() {
+	for _, cleanup := range sm.cleaners {
 		cleanup()
 	}
-	for _, stream := range fdt.mappings {
+	for _, stream := range sm.mappings {
 		stream.Close()
 	}
 }
 
-func (fdt *StreamManager) Clone() (*StreamManager, error) {
+func (sm *StreamManager) Clone() (*StreamManager, error) {
 	return &StreamManager{
-		parent:   fdt,
+		parent:   sm,
 		mappings: make(map[string]Stream),
 	}, nil
 }
