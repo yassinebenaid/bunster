c42030b7d81f18110c09091543f7f253ad5e2dd5
yassinebenaid <yassinebenaide3@gmail.com>
2025-02-09 12:55:59 +0100

    add parser support for 'export' keyword

--- parser/parameter_assignment_test.go
+++ parser/parameter_assignment_test.go
@@ -69,9 +69,44 @@ var parameterAssignmentTests = []testCase{
 			ast.Assignement{Name: "var"},
 		},
 	}},
+
+	{`export var=value var2='value2' var3="value3"`, ast.Script{
+		ast.ExportParameterAssignement{
+			ast.Assignement{Name: "var", Value: ast.Word("value")},
+			ast.Assignement{Name: "var2", Value: ast.Word("value2")},
+			ast.Assignement{Name: "var3", Value: ast.Word("value3")},
+		},
+	}},
+	{`export var=$var var=${var}`, ast.Script{
+		ast.ExportParameterAssignement{
+			ast.Assignement{Name: "var", Value: ast.Var("var")},
+			ast.Assignement{Name: "var", Value: ast.Var("var")},
+		},
+	}},
+	{`export var= var2=value var3=`, ast.Script{
+		ast.ExportParameterAssignement{
+			ast.Assignement{Name: "var"},
+			ast.Assignement{Name: "var2", Value: ast.Word("value")},
+			ast.Assignement{Name: "var3"},
+		},
+	}},
+	{`export var var2 var3`, ast.Script{
+		ast.ExportParameterAssignement{
+			ast.Assignement{Name: "var"},
+			ast.Assignement{Name: "var2"},
+			ast.Assignement{Name: "var3"},
+		},
+	}},
+	{`export var # comment`, ast.Script{
+		ast.ExportParameterAssignement{
+			ast.Assignement{Name: "var"},
+		},
+	}},
 }
 
 var parameterAssignmentErrorHandlingCases = []errorHandlingTestCase{
 	{`local`, "syntax error: unexpected token `end of file`. (line: 1, column: 6)"},
 	{`local # variable`, "syntax error: unexpected token `end of file`. (line: 1, column: 17)"},
+	{`export`, "syntax error: unexpected token `end of file`. (line: 1, column: 7)"},
+	{`export # variable`, "syntax error: unexpected token `end of file`. (line: 1, column: 18)"},
 }
