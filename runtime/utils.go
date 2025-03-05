36b3e35426ac3079d56615dd2ab76f33cbf8eaf2
yassinebenaid <yassinebenaide3@gmail.com>
2025-03-05 07:46:51 +0000

    fix syscall not available on darwin

--- runtime/utils.go
+++ runtime/utils.go
@@ -4,7 +4,6 @@ import (
 	"os"
 	"strconv"
 	"syscall"
-	"unsafe"
 )
 
 func NumberCompare(x, op, y string) bool {
@@ -177,24 +176,4 @@ func FileHasAPositiveSize(file string) bool {
 	}
 
 	return stat.Size > 0
-}
-
-func FileDescriptorIsTerminal(sm *StreamManager, fd string) bool {
-	original, err := sm.Get(fd)
-	if err != nil {
-		return false
-	}
-
-	file, ok := original.(interface{ Fd() uintptr })
-	if !ok {
-		return false
-	}
-
-	_, _, errno := syscall.Syscall(
-		syscall.SYS_IOCTL,
-		file.Fd(),
-		syscall.TCGETS,
-		uintptr(unsafe.Pointer(&syscall.Termios{})),
-	)
-	return errno == 0
 }
---
+++ runtime/utils_darwin.go
@@ -0,0 +1,28 @@
+//go:build darwin
+
+package runtime
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+func FileDescriptorIsTerminal(sm *StreamManager, fd string) bool {
+	original, err := sm.Get(fd)
+	if err != nil {
+		return false
+	}
+
+	file, ok := original.(interface{ Fd() uintptr })
+	if !ok {
+		return false
+	}
+
+	_, _, errno := syscall.Syscall(
+		syscall.SYS_IOCTL,
+		file.Fd(),
+		syscall.TIOCGETA,
+		uintptr(unsafe.Pointer(&syscall.Termios{})),
+	)
+	return errno == 0
+}
