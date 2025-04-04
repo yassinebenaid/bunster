1fa7b93810ae10565d169b48cedf7b12776e58bf
yassinebenaid <yassinebenaide3@gmail.com>
2025-04-04 14:04:02 +0000

    package management: bugfix

--- builder/query_test.go
+++ builder/query_test.go
@@ -18,12 +18,12 @@ func TestParserQuery(t *testing.T) {
 		query query
 		err   string
 	}{
-		{input: "", err: "module path is not in an expected format"},
-		{input: "foo", err: "module path is not in an expected format"},
-		{input: "foo/bar", err: "module path is not in an expected format"},
-		{input: "foo.com/bar/baz", err: "module path is not in an expected format"},
-		{input: "foo.com/bar/baz@", err: "module path is not in an expected format"},
-		{input: "foo.com/bar/baz@bad-commit-hash", err: "module path is not in an expected format"},
+		{input: "", err: `module path "" is not in an expected format`},
+		{input: "foo", err: `module path "foo" is not in an expected format`},
+		{input: "foo/bar", err: `module path "foo/bar" is not in an expected format`},
+		{input: "foo.com/bar/baz", err: `module path "foo.com/bar/baz" is not in an expected format`},
+		{input: "foo.com/bar/baz@", err: `module path "foo.com/bar/baz@" is not in an expected format`},
+		{input: "foo.com/bar/baz@bad-commit-hash", err: `module path "foo.com/bar/baz@bad-commit-hash" is not in an expected format`},
 		{
 			input: "foo.com/bar/baz@a86d7b5f7d93c8a6263979dc9e7a2beb1124acce",
 			query: query{
